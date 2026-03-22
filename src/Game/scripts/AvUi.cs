a1ff04a7b497b886f32b6ccae22b57701cffb54b
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-03-21 20:53:06 -0700

    Rename Copper DC files
    Signed-off-by: SubParSuperCar <elliot_grimm_2008@yahoo.com>

--- src/Game/scripts/AvUi.cs
+++ src/Game/scripts/AvUi.cs
@@ -6,7 +6,7 @@ using Godot;
 // ReSharper disable once CheckNamespace
 namespace Game;
 
-public sealed partial class AvUI : AvControl
+public sealed partial class AvUi : AvControl
 {
 	private MainViewModel _mainViewModel = null!;
 
